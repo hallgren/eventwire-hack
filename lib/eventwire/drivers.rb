2ab6e81368b3e7e99ef2231e0f56368f048eff60
Morgan Hallgren <morgan.hallgren@gmail.com>
2011-12-13 13:40:01 +0100

    Removed drivers that use standard interface

--- lib/eventwire/drivers.rb
+++ lib/eventwire/drivers.rb
@@ -2,11 +2,6 @@ require 'json'
 
 module Eventwire
   module Drivers
-    autoload :InProcess, 'eventwire/drivers/in_process'
-    autoload :AMQP,      'eventwire/drivers/amqp'
     autoload :AMQP_UPPTEC,      'eventwire/drivers/amqp_upptec'
-    autoload :Bunny,     'eventwire/drivers/bunny'
-    autoload :Redis,     'eventwire/drivers/redis'
-    autoload :Zero,      'eventwire/drivers/zero'
   end
 end
